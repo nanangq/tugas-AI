db9bb0cd85a32ff9211a5ff316608e95b2172cf1
nanangq <80019422+nanangq@users.noreply.github.com>
2021-11-07 12:30:10 +0700

    Update OpenCVimage.py

--- OpenCVimage.py
+++ OpenCVimage.py
@@ -5,4 +5,5 @@ def rescale(frame, scale=0.75):
     dimension = (int(frame.shape[1]*scale),int(frame.shape[0]*scale))
     return cv.resize(frame, dimension,interpolation=cv.INTER_AREA) 
 cv.imshow('Daun Nangka', rescale(cv.imread('dataset/nangka/001.jpg')))
+cv.imshow('Daun Sirih',  rescale(cv.imread('dataset/sirih/001.jpg')))
 cv.waitKey(0)
